97c6770cf915746150985627cb5090436a78cee1
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-05 20:25:49 +0530

    update constants file  variables

--- instabot/constants.py
+++ instabot/constants.py
@@ -1,4 +1,5 @@
 import requests
-
-APP_ACCESS_TOKEN = '4631267286.cOOe824.bOabe1e188f8488fbe0966f5d7d25b88'
-BASE_URL = 'https://api.instagram.com/v1/'
+#instagram API access token
+app_access_token = '4631267286.cOOe824.bOabe1e188f8488fbe0966f5d7d25b88'
+#instagram base url
+base_url = 'https://api.instagram.com/v1/'
