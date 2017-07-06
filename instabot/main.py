f3f71e99b15cc0a6109c1677dfa7209fe86c8aaf
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-06 16:37:30 +0530

    use the get_own_post function in the main file

--- instabot/main.py
+++ instabot/main.py
@@ -1,4 +1,7 @@
 #fetching own information
 from self_info import self_info
+#fetching our own data
+from get_own_post import get_own_post
 self_info()
-#call the function
+result=get_own_post()
+print result
