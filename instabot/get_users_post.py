f6940d4fa31cc13ea1c276b5068cf68cd7b7969b
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-08 16:17:15 +0530

    get_user_post function update again

--- instabot/get_users_post.py
+++ instabot/get_users_post.py
@@ -1,6 +1,7 @@
 import requests
 from get_user_id import get_user_id
 from constants import app_access_token,base_url
+import urllib.urlretrieve
 
 def get_users_post(insta_username):
     #function logic
@@ -8,16 +9,20 @@ def get_users_post(insta_username):
     if user_id == None:
         print 'User does not exist!'
         exit()
-        request_url = (base_url + 'users/%s/media/recent/?access_token=%s') % (user_id, app_access_token)
-        print 'GET request url : %s' % (request_url)
-        user_media = requests.get(request_url).json()
+    request_url = (base_url + 'users/%s/media/recent/?access_token=%s') % (user_id, app_access_token)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
 
-        if user_media['meta']['code'] == 200:
+    if user_media['meta']['code'] == 200:
             if len(user_media['data']):
-                return user_media['data'][0]['id']
+                image_name = user_media['data'][0]['id'] + '.jpeg'
+                image_url = user_media['data'][0]['images']['standard_resolution']['url']
+                urllib.urlretrieve(image_url, image_name)
+                print 'Your image has been downloaded!'
+
             else:
-                print "There is no recent post!"
-        else:
+                print "post does not exist!"
+    else:
             print "Status code other than 200 received!"
-        return None
+
 
