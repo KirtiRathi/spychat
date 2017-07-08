f5176fbb4a3c0537197cd545156da3b383672cc3
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-08 17:19:34 +0530

    like_a_post function allow to loke a post

---
+++ instabot/like_a_post.py
@@ -0,0 +1,10 @@
+import requests
+from constants import app_access_token,base_url
+from get_post_id import get_post_id
+
+def like_a_post(insta_username):
+  media_id = get_post_id(insta_username)
+  request_url = (base_url + 'media/%s/likes') % (media_id)
+  payload = {"access_token": app_access_token}
+  print 'POST request url : %s' % (request_url)
+  post_a_like = requests.post(request_url, payload).json()
