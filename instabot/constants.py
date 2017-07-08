e8d4b02352a1272b9d92739297a6ff866e5085a1
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-08 17:46:04 +0530

    added the textblob in the constants file

--- instabot/constants.py
+++ instabot/constants.py
@@ -1,5 +1,7 @@
-import requests,urllib
+import requests
 #instagram API access token
 app_access_token = '4631267286.cOOe824.bOabe1e188f8488fbe0966f5d7d25b88'
 #instagram base url
 base_url = 'https://api.instagram.com/v1/'
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
