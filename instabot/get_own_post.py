dda6881638ce17f4b184394c2fea260b19be8f05
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-08 16:08:42 +0530

    get_own_post function update

--- instabot/get_own_post.py
+++ instabot/get_own_post.py
@@ -1,5 +1,6 @@
 import requests
 from constants import app_access_token,base_url
+import urllib.urlretrieve
 
 def get_own_post():
     #https: // api.instagram.com / v1 / users / self / media / recent /?access_token = 4631267286.cOOe824.bOabe1e188f8488fbe0966f5d7d25b88
@@ -11,9 +12,12 @@ def get_own_post():
 
         if own_media['meta']['code'] == 200:
             if len(own_media['data']):
-                return own_media['data'][0]['id']
+                    image_name = own_media['data'][0]['id'] + '.jpeg'
+                    image_url = own_media['data'][0]['images']['standard_resolution']['url']
+                    urllib.urlretrieve(image_url, image_name)
+                    print 'Your image has been downloaded!'
+
             else:
                 print 'Post does not exist!'
         else:
             print 'Status code other than 200 received!'
-            return None
--- instabot/get_users_post.py
+++ instabot/get_users_post.py
@@ -11,6 +11,7 @@ def get_users_post(insta_username):
         request_url = (base_url + 'users/%s/media/recent/?access_token=%s') % (user_id, app_access_token)
         print 'GET request url : %s' % (request_url)
         user_media = requests.get(request_url).json()
+
         if user_media['meta']['code'] == 200:
             if len(user_media['data']):
                 return user_media['data'][0]['id']
@@ -19,3 +20,4 @@ def get_users_post(insta_username):
         else:
             print "Status code other than 200 received!"
         return None
+
