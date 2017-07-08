99b71b10d813ea73e121c01c0a767cf0f7de4201
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-08 16:36:20 +0530

    main file include a function

---
+++ instabot/demo.py
@@ -0,0 +1,26 @@
+# elif choice=="e":
+#    insta_username = raw_input("Enter the username of the user: ")
+#    get_like_list(insta_username)
+# elif choice=="f":
+#    insta_username = raw_input("Enter the username of the user: ")
+#    like_a_post(insta_username)
+# elif choice=="g":
+#    insta_username = raw_input("Enter the username of the user: ")
+#    get_comment_list(insta_username)
+# elif choice=="h":
+#    insta_username = raw_input("Enter the username of the user: ")
+#    make_a_comment(insta_username)
+# elif choice=="i":
+#    insta_username = raw_input("Enter the username of the user: ")
+#    delete_negative_comment(insta_username)
+
+
+
+
+
+# print "e.Get a list of people who have liked the recent post of a user\n"
+        # print "f.Like the recent post of a user\n"
+        # print "g.Get a list of comments on the recent post of a user\n"
+        # print "h.Make a comment on the recent post of a user\n"
+        # print "i.Delete negative comments from the recent post of a user\n"
+        print "j.Exit"
--- instabot/main.py
+++ instabot/main.py
@@ -2,6 +2,39 @@
 from self_info import self_info
 #fetching our own data
 from get_own_post import get_own_post
-self_info()
-result=get_own_post()
-print result
+#fetching user information
+from get_user_info import get_user_info
+#fetching user post
+from get_users_post import get_users_post
+
+
+def start_bot():
+    while True:
+        print '\n'
+        print 'Hey! Welcome to instaBot!'
+        print 'Here are your menu options:'
+        print "a.Get your own details\n"
+        print "b.Get details of a user by username\n"
+        print "c.Get your own recent post\n"
+        print "d.Get the recent post of a user by username\n"
+
+        print "j.Exit"
+
+        choice = raw_input("Enter you choice: ")
+        if choice == "a":
+            self_info()
+        elif choice == "b":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_info(insta_username)
+        elif choice == "c":
+            get_own_post()
+        elif choice == "d":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_users_post(insta_username)
+
+        elif choice == "j":
+            exit()
+        else:
+            print "wrong choice"
+
+start_bot()
