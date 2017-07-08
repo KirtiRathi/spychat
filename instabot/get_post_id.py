1ce801bd9d346c1f2ef08127a46e68c66223c6bb
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-08 17:16:40 +0530

    get_post_id function return the post id

---
+++ instabot/get_post_id.py
@@ -0,0 +1,22 @@
+import requests
+from get_user_id import get_user_id
+from constants import app_access_token,base_url
+def get_post_id(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (base_url + 'users/%s/media/recent/?access_token=%s') % (user_id, app_access_token)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            return user_media['data'][0]['id']
+        else:
+            print 'There is no recent post of the user!'
+            exit()
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
