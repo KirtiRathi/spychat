5a302187ee6a0efbcf798cf70a4575f5f6e4087e
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-08 15:47:01 +0530

    get_users_post function update

--- instabot/get_users_post.py
+++ instabot/get_users_post.py
@@ -8,6 +8,14 @@ def get_users_post(insta_username):
     if user_id == None:
         print 'User does not exist!'
         exit()
-        request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+        request_url = (base_url + 'users/%s/media/recent/?access_token=%s') % (user_id, app_access_token)
         print 'GET request url : %s' % (request_url)
         user_media = requests.get(request_url).json()
+        if user_media['meta']['code'] == 200:
+            if len(user_media['data']):
+                return user_media['data'][0]['id']
+            else:
+                print "There is no recent post!"
+        else:
+            print "Status code other than 200 received!"
+        return None
