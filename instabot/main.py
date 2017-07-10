ade92ad0d4f3260c6c604f30b972110839346827
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-10 16:21:32 +0530

    update the main file

--- instabot/main.py
+++ instabot/main.py
@@ -6,6 +6,12 @@ from get_own_post import get_own_post
 from get_user_info import get_user_info
 #fetching user post
 from get_users_post import get_users_post
+#for like a post
+from like_a_post import like_a_post
+# for post a comment
+from post_a_comment import post_a_comment
+#for delete the negative comment
+from delete_negative_comment import delete_negative_comment
 
 
 def start_bot():
@@ -17,10 +23,15 @@ def start_bot():
         print "b.Get details of a user by username\n"
         print "c.Get your own recent post\n"
         print "d.Get the recent post of a user by username\n"
-
+        print "e.Get a list of people who have liked the recent post of a user\n"
+        print "f.Like the recent post of a user\n"
+        print "g.Get a list of comments on the recent post of a user\n"
+        print "h.Make a comment on the recent post of a user\n"
+        print "i.Delete negative comments from the recent post of a user\n"
         print "j.Exit"
 
         choice = raw_input("Enter you choice: ")
+
         if choice == "a":
             self_info()
         elif choice == "b":
@@ -31,7 +42,21 @@ def start_bot():
         elif choice == "d":
             insta_username = raw_input("Enter the username of the user: ")
             get_users_post(insta_username)
-
+        #elif choice=="e":
+         #  insta_username = raw_input("Enter the username of the user: ")
+          # get_like_list(insta_username)
+        elif choice=="f":
+           insta_username = raw_input("Enter the username of the user: ")
+           like_a_post(insta_username)
+        #elif choice=="g":
+         #  insta_username = raw_input("Enter the username of the user: ")
+          # get_comment_list(insta_username)
+        elif choice=="h":
+           insta_username = raw_input("Enter the username of the user: ")
+           post_a_comment(insta_username)
+        elif choice=="i":
+           insta_username = raw_input("Enter the username of the user: ")
+           delete_negative_comment(insta_username)
         elif choice == "j":
             exit()
         else:
