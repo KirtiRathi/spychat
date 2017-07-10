bbadadd550e5bd6031268610ecb672610df915cc
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-10 16:17:10 +0530

    again update the constant file

--- instabot/constants.py
+++ instabot/constants.py
@@ -1,7 +1,9 @@
 import requests
 #instagram API access token
-app_access_token = '4631267286.cOOe824.bOabe1e188f8488fbe0966f5d7d25b88'
+app_access_token = '4631267286.c00e824.b0abe1e188f8488fbe0966f5d7d25b88'
 #instagram base url
 base_url = 'https://api.instagram.com/v1/'
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
+
+
