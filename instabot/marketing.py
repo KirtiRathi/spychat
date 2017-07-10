0ef3dc763a983ed320cc971c1b93d9dd30de51c2
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-10 21:06:28 +0530

    target_a_comment function are created for marketing a product

---
+++ instabot/marketing.py
@@ -0,0 +1,24 @@
+import  requests
+import urllib
+from constants import app_access_token,base_url
+from get_user_id import get_user_id
+from post_a_comment import post_a_comment
+
+insta_username="jyotithakur15111"
+
+def target_a_comment(insta_username):
+    user_id=get_user_id(insta_username)
+    request_url=(base_url+'user/%s/media/recent/?access_token=%s')%(user_id,app_access_token)
+
+    print 'GET request url : %s'%(request_url)
+    user_media=requests.get(request_url).json()
+
+    service=['clothes','coffee','ice-cream','pizza']
+    for post in user_media['data']:
+        for serve in service:
+            if service in post ['caption']['text']:
+                post_a_comment(insta_username)
+
+            else:
+                print'Error'
+target_a_comment(insta_username)
