fbab9d0bec7bc244f2ecbad11abf3cd96d7e10e7
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-11 13:44:58 +0530

    update the target_a_comment function again

--- instabot/marketing.py
+++ instabot/marketing.py
@@ -1,26 +1,31 @@
-import  requests
+import requests
 import urllib
 from constants import app_access_token,base_url
 from get_user_id import get_user_id
 from post_a_comment import post_a_comment
 
-#insta_username="jyotithakur15111"
+
+
 
 
 def target_a_comment(insta_username):
     #function of logic
     user_id=get_user_id(insta_username)
-    request_url=(base_url+'user/%s/media/recent/?access_token=%s')%(user_id,app_access_token)
-
-    print 'GET request url : %s'%(request_url)
-    user_media=requests.get(request_url).json()
-
-    service=['clothes','coffee','ice-cream','pizza']
+    print user_id
+    request_url = (base_url+'users/%s/media/recent/?access_token=%s')%(user_id,app_access_token)
+    print 'GET request url : %s' %(request_url)
+    user_media = requests.get(request_url).json()
+    print user_media
+    service = ['clothes','coffee','ice-cream','pizza']
     for post in user_media['data']:
-        for serve in service:
-            if service in post ['caption']['text']:
-                post_a_comment(insta_username)
+        print post
 
+        for serve in service:
+            if post['caption'] == None:
+                print "This post has no caption"
             else:
-                print'Error'
-target_a_comment(insta_username)
+                if serve in post['caption']['text']:
+                    post_a_comment(insta_username)
+
+
+#target_a_comment(insta_username)
