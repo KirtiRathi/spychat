98c8cb51d6a35f58265d3c7a35dc8e5cb79f3f29
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-11 11:23:38 +0530

    update the like_a_post file

--- instabot/like_a_post.py
+++ instabot/like_a_post.py
@@ -3,8 +3,10 @@ from constants import app_access_token,base_url
 from get_post_id import get_post_id
 
 def like_a_post(insta_username):
+  #function of logic
   media_id = get_post_id(insta_username)
   request_url = (base_url + 'media/%s/likes') % (media_id)
   payload = {"access_token": app_access_token}
   print 'POST request url : %s' % (request_url)
   post_a_like = requests.post(request_url, payload).json()
+  print 'post liked sucessfully'
