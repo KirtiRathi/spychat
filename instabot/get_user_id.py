5f615207e9625870a89a8a2ea81e0339bf46fba5
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-11 13:41:52 +0530

    update the get_user_id again

--- instabot/get_user_id.py
+++ instabot/get_user_id.py
@@ -2,14 +2,17 @@ import requests
 from constants import app_access_token,base_url
 def get_user_id(insta_username):
     #function of logic
+
     request_url = (base_url + 'users/search?q=%s&access_token=%s') % (insta_username, app_access_token)
     print 'GET request url : %s' % (request_url)
     user_info = requests.get(request_url).json()
     if user_info['meta']['code'] == 200:
         if len(user_info['data']):
+            print user_info['data'][0]['id']
             return user_info['data'][0]['id']
         else:
-            return None
+            print "User not found"
+            exit(0)
     else:
         print 'Status code other than 200 received!'
-        exit()
+        exit(0)
