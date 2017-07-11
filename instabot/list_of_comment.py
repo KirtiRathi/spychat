ca127cc35b7680c91d02f780880ae4772875a63c
KirtiRathi <kirtirathimca006@gmail.com>
2017-07-11 20:55:37 +0530

    update the code of file

--- instabot/list_of_comment.py
+++ instabot/list_of_comment.py
@@ -1,15 +1,16 @@
-import requests
+
 from constants import app_access_token,base_url
 from get_post_id import get_post_id
+from post_a_comment import post_a_comment
 
 def list_of_comment(insta_username):
     media_id=get_post_id(insta_username)
-    request_url=(base_url+'media/%s/comment?access_token=$s')%(media_id,app_access_token)
+    request_url=(base_url+'media/%s/comments?access_token=%s')%(media_id,app_access_token)
     print 'Get request url :%s'%(request_url)
-    get_a_comment=requests.get(request_url).json()
-    if get_a_comment['meta']['code']==200:
-        print "media with media id '()' is comment by following user:".format(media_id)
-        for (index,user_comment)in enumerate(get_a_comment['data']):
-            print "{ }.{ } ({ }) -{}".format(index+1,user_comment['full_name'],user_comment['id'],user_comment['username'])
+
+    if post_a_comment(insta_username)['meta']['code']==200:
+        print "media with media id '()' is comment by following users:".format(media_id)
+        for (index,users_comments)in enumerate(post_a_comment()['data']):
+            print "%d. %s (%s) - %d "% (index+1,users_comments['full_name':],users_comments['id':],users_comments['username':])
     else:
         print "wrong input plzz try again...."
